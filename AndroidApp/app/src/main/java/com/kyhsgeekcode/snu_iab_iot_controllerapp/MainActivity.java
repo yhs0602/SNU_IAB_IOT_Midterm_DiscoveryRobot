11871689cf58b570b4fae7ae0fb2630ba34752af
KYHSGeekCode <jourhyang123@gmail.com>
2019-10-17 13:02:08 +0900

    Added UI components

--- AndroidApp/app/src/main/java/com/kyhsgeekcode/snu_iab_iot_controllerapp/MainActivity.java
+++ AndroidApp/app/src/main/java/com/kyhsgeekcode/snu_iab_iot_controllerapp/MainActivity.java
@@ -4,11 +4,72 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import tech.gusavila92.websocketclient.WebSocketClient;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    private WebSocketClient webSocketClient;
+
+    private void createWebSocketClient() {
+        URI uri;
+        try {
+            uri = new URI("ws://192.168.4.1:81/test");
+        }
+        catch (URISyntaxException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        webSocketClient = new WebSocketClient(uri) {
+            @Override
+            public void onOpen() {
+                System.out.println("onOpen");
+                webSocketClient.send("Hello, World!");
+            }
+
+            @Override
+            public void onTextReceived(String message) {
+                System.out.println("onTextReceived");
+            }
+
+            @Override
+            public void onBinaryReceived(byte[] data) {
+                System.out.println("onBinaryReceived");
+            }
+
+            @Override
+            public void onPingReceived(byte[] data) {
+                System.out.println("onPingReceived");
+            }
+
+            @Override
+            public void onPongReceived(byte[] data) {
+                System.out.println("onPongReceived");
+            }
+
+            @Override
+            public void onException(Exception e) {
+                System.out.println(e.getMessage());
+            }
+
+            @Override
+            public void onCloseReceived() {
+                System.out.println("onCloseReceived");
+            }
+        };
+
+        webSocketClient.setConnectTimeout(10000);
+        webSocketClient.setReadTimeout(60000);
+        webSocketClient.addHeader("arduino", "http://developer.example.com");
+        webSocketClient.enableAutomaticReconnection(5000);
+        webSocketClient.connect();
     }
 }
