f15dc01feaa334fd07e0d9dcc8cdda1ce59e57fe
KYHSGeekCode <jourhyang123@gmail.com>
2019-10-17 11:01:35 +0900

    App Initial commit

---
+++ AndroidApp/app/src/main/java/com/kyhsgeekcode/snu_iab_iot_controllerapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.kyhsgeekcode.snu_iab_iot_controllerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
